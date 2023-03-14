f340db7d4fd7e138973386e6e68fc58182e9f883
Ashot Abazyan <ashot.93@mail.ru>
2023-03-14 22:06:39 +0400

    Recreated app

--- food_app/apps.py
+++ food_app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FoodConfig(AppConfig):
+class FoodAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'food'
+    name = 'food_app'
--- food_plan/urls.py
+++ food_plan/urls.py
@@ -1,4 +1,4 @@
-"""foodplan URL Configuration
+"""food_plan URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
