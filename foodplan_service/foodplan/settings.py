d514a6ed3e1ae7710bfc1a7e2f338d8d04545f10
Ashot Abazyan <ashot.93@mail.ru>
2023-03-14 21:57:42 +0400

    added app

--- foodplan_service/foodplan/settings.py
+++ foodplan_service/foodplan/settings.py
@@ -74,20 +74,10 @@ WSGI_APPLICATION = 'foodplan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
