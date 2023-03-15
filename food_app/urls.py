057b1059683c6e80876271ad4376aefc1dfa59e5
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-03-15 16:06:18 +0300

    feature(pages): add registration/auth/order/account pages

--- food_app/urls.py
+++ food_app/urls.py
@@ -1,3 +1,4 @@
+from django.conf.locale import vi
 from django.urls import path
 
 from food_app import views
@@ -6,4 +7,6 @@ app_name = 'food_app'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('order/', views.order, name='order'),
+    path('account/', views.account, name='account'),
 ]
--- food_app/views.py
+++ food_app/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'food_app/pages/index.html')
+
+def order(request):
+    return render(request, 'food_app/pages/order.html')
+
+def account(request):
+    return render(request, 'food_app/pages/account.html')
