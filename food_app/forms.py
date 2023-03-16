71a25a4ce38f86ff056ab976b731e5ce9f04ca68
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-03-16 15:06:37 +0300

    feature(form): add payment from

--- food_app/forms.py
+++ food_app/forms.py
@@ -24,3 +24,71 @@ class OrderForm(forms.Form):
         choices=[(count, count) for count in range(1, 7)],
         widget=forms.Select(attrs={'class': 'form-select'})
     )
+
+
+class PaymentForm(forms.Form):
+    card_name = forms.CharField(min_length=2, label='Имя владельца')
+    card_number = forms.CharField(min_length=16, max_length=16, label='Номер карты')
+    card_month = forms.IntegerField(min_value=1, max_value=12, label='Месяц')
+    card_day = forms.IntegerField(min_value=1, max_value=31, label='День')
+    card_cvc = forms.CharField(min_length=3, max_length=3, label='CVC')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Field attrs
+        self.fields['card_name'].widget.attrs.update(
+            {
+                'class': 'form-control',
+                'placeholder': 'Имя Фамилия',
+                'type': 'text',
+                'id': 'card-name',
+                'pattern': '[a-zA-Z ]+',
+            }
+        )
+        self.fields['card_number'].widget.attrs.update(
+            {
+                'class': 'form-control',
+                'placeholder': '1234 5678 9123 4561',
+                'type': 'number',
+                'id': 'card-number',
+                'oninput': 'this.value = this.value.slice(0, 16)',
+                'min': '0',
+            }
+        )
+        self.fields['card_month'].widget.attrs.update(
+            {
+                'class': 'form-control',
+                'placeholder': 'MM',
+                'type': 'number',
+                'id': 'expiry-date-month',
+                'pattern': '(0[1-9]|1[0-2])\/[0-9]{2}',
+                'oninput': 'this.value = this.value.slice(0, 2)',
+            }
+        )
+        self.fields['card_day'].widget.attrs.update(
+            {
+                'class': 'form-control',
+                'placeholder': 'ДД',
+                'type': 'number',
+                'id': 'expiry-date-day',
+                'pattern': '(0[1-9]|1[0-2])\/[0-9]{2}',
+                'oninput': 'this.value = this.value.slice(0, 2)',
+            }
+        )
+        self.fields['card_cvc'].widget.attrs.update(
+            {
+                'class': 'form-control',
+                'placeholder': '123',
+                'type': 'number',
+                'id': 'cvc',
+                'oninput': 'this.value = this.value.slice(0, 3)',
+            }
+        )
+
+        # Label attrs
+        self.fields['card_name'].label_attrs = {'class': 'form-label'}
+        self.fields['card_number'].label_attrs = {'class': 'form-label'}
+        self.fields['card_month'].label_attrs = {'class': 'form-label'}
+        self.fields['card_day'].label_attrs = {'class': 'form-label'}
+        self.fields['card_cvc'].label_attrs = {'class': 'form-label'}
