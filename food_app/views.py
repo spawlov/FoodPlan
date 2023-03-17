8a259bfb8ae1f1ed2e1531ff29a49253975ec312
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-03-17 21:13:50 +0300

    feature(page): add order page(some functionality)'

--- food_app/views.py
+++ food_app/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from .models import Customer
+from calendar import mdays
+from datetime import datetime, timedelta
+
+from food_app.forms import OrderForm
+
+from .models import Customer, Plan, Subscription
 
 
 def index(request):
@@ -34,3 +39,41 @@ def account(request):
             customer.avatar = request.FILES['avatar']
             customer.save()
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def order(request):
+    if request.method == 'GET':
+        order_form = OrderForm()
+        return render(request, 'food_app/pages/order.html', {'order_form': order_form})
+
+    if request.method == 'POST':
+        order_form = OrderForm(request.POST)
+
+        if not order_form.is_valid():
+            return render(request, 'food_app/pages/order.html', {'order_form': order_form})
+
+        plan = Plan(
+            price=5,
+            period=order_form.cleaned_data['period'],
+            recipe_category=order_form.cleaned_data['recipe_categories']
+        )
+        plan.save()
+        plan.allergies.add(*order_form.cleaned_data['allergies'])
+        plan.save()
+
+        today = datetime.now()
+        subscription_end = today + timedelta(mdays[today.month])
+
+        subscription = Subscription(
+            end=subscription_end,
+            plan=plan,
+        )
+        subscription.save()
+        customer = Customer.objects.get(user=request.user)
+        customer.subscriptions.add(subscription)
+        customer.save()
+
+        return redirect(request, 'food_plan:index')
+
+
+
