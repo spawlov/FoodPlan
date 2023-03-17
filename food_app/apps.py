f443eb0629394e6f85a173cf1202cbc7366afc78
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-03-18 01:43:53 +0300

    feature(app_name): change the name for the food_app

--- food_app/apps.py
+++ food_app/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class FoodAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'food_app'
+    name = 'Food for Life'
 
     def ready(self):
         from . import signals
