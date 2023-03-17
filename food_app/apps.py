96bd69af66dad234a01819d13ec7dbeebfd21219
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-03-18 02:00:40 +0300

    fix(app config): fix name, add verbose name

--- food_app/apps.py
+++ food_app/apps.py
@@ -3,7 +3,8 @@ from django.apps import AppConfig
 
 class FoodAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Food for Life'
+    name = 'food_app'
+    verbose_name = 'Food for Life'
 
     def ready(self):
         from . import signals
