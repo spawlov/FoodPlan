87174e9570d82e7abfa791c97a86d006568ef7a4
Alexander Samokhvalov <alexshikari9333@gmail.com>
2023-03-18 18:23:12 +0300

    feature(template): display errors in the order form

--- food_app/forms.py
+++ food_app/forms.py
@@ -35,8 +35,6 @@ class OrderForm(forms.ModelForm):
         self.fields['period'].empty_label = None
         self.fields['food_intakes'].empty_label = None
 
-        self.fields['allergies'].required = True
-
 
 class PaymentForm(forms.Form):
     card_name = forms.CharField(min_length=2, label='Имя владельца')
