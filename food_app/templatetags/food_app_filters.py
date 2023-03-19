a570511d2cb870f5a83d5632ed32e34588846dbf
spawlov <spawlov@gmail.com>
2023-03-20 04:50:23 +0700

    Refactoring food_app_filters.py

--- food_app/templatetags/food_app_filters.py
+++ food_app/templatetags/food_app_filters.py
@@ -1,11 +1,12 @@
-import datetime
+from datetime import timedelta
 
 from django import template
+from django.utils import timezone
 
 register = template.Library()
 
 
 @register.filter()
 def add_days(days):
-    new_date = datetime.date.today() + datetime.timedelta(days=days)
+    new_date = timezone.now() + timedelta(days=days)
     return new_date
