d6f46def98c75762d5d1ad081062194ebe43a6cf
sergey <spawlov@gmail.com>
2024-02-06 06:29:44 +0700

    Update settings.py

--- food_plan/settings.py
+++ food_plan/settings.py
@@ -17,13 +17,11 @@ from environs import Env
 
 from yookassa import Configuration
 
-
 env = Env()
 env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -39,7 +37,7 @@ ALLOWED_HOSTS = env.list(
     default=['localhost', '127.0.0.1', '[::1]'],
 )
 
-CSRF_TRUSTED_ORIGINS=env.list('CSRF_ORIGINS', default=[])
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
 
 # Application definition
 
@@ -89,7 +87,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'food_plan.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -99,7 +96,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -119,7 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -133,7 +128,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -144,12 +138,10 @@ STATICFILES_DIRS = []
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 # Allauth block
 # https://django-allauth.readthedocs.io/en/latest/index.html
@@ -288,7 +280,6 @@ CKEDITOR_5_CONFIGS = {
 
 APSCHEDULER_DATETIME_FORMAT = "d.m.Y, H:i:s"
 APSCHEDULER_RUN_NOW_TIMEOUT = 25
-
 
 # Yookassa
 Configuration.account_id = env('YOOKASSA_SHOP_ID', default="")
